347647459994442d2da151c564cb27107c2afa0f
Alberto Vidana <alberto.vidana@gmail.com>
2025-05-25 22:39:28 -0600

    Encaptulate lesson completed #SCALA

--- Introduction to Object-Oriented Programming in Scala/Encapsulation in Scala: Implementing Data Protection and Control/EncapsulatingSavingsInScalaBuildingAPiggyBankClass.scala
+++ Introduction to Object-Oriented Programming in Scala/Encapsulation in Scala: Implementing Data Protection and Control/EncapsulatingSavingsInScalaBuildingAPiggyBankClass.scala
@@ -11,16 +11,21 @@
   */
 
   class PiggyBank:
-    println()
     // TODO: Define a private variable to store the amount in the piggy bank
-    
+    private var balance: Double = 0.0
     // TODO: Create an "addMoney" method that validates and adds money to the piggy bank, and then prints out the amount added
-   
+    def addMoney(value: Double): Unit =
+        if value > 0 then
+            balance += value
+            showBalance()
     // TODO: Create a "showBalance" method to display the balance of the piggy bank without exposing the amount variable
+    def showBalance(): Unit =
+        println(s"Current balance is: $balance")
 
 @main def run: Unit =
     // TODO: Create an instance of PiggyBank
-    
+    val piggyBank = PiggyBank()
     // TODO: Call the addMoney method with a positive amount
-    
+    piggyBank.addMoney(100.00)
     // TODO: Call the showBalance method to display current balance
+    piggyBank.showBalance()
